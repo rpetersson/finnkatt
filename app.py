3e9baec23462a240a54b63ef427edab385dbeb33
robert petersson <rpetersson@gmail.com>
2024-12-29 14:39:36 +0100

    update

--- app.py
+++ app.py
@@ -54,34 +54,5 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', form=form, googlemaps_key=os.getenv('GOOGLEMAPS_KEY'))
 
-@app.route('/map')
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return render_template('map.html', mymap=mymap, sndmap=sndmap)
 if __name__ == '__main__':
     app.run(debug=True)
